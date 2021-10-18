0af009f5ff306c06405003c40378de42d6e7f554
indahputris <indahputrisnh@gmail.com>
2021-10-18 16:23:54 +0700

    first commit

---
+++ application/views/v_about.php
@@ -0,0 +1,21 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Indah</li>
+        <li>Nama Belakang : Putri Suryaningsih</li>
+    </ul>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Jalan Karet Pasar Baru Barat II No.17 RT 06 RW 05 Kelurahan Karet Tengsin Kecamatan Tanah Abang Jakarta Pusat</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Volly</li>
+        <li>Futsal</li>
+    </ul>
+</section>
---
+++ application/views/view-latihan1.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    <br>Halo Kawan.. Yuk kita belajar web programming..!!!</br>
+    <br>Nilai 1 = <?= $nilai1; ?></br>
+    <br>Niali 2 = <?= $nilai2; ?></br>
+    <br>Hasil dari permodelan dengan methode penjumlahan yaitu
+    <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?> </br>
+</body>
+</html>
