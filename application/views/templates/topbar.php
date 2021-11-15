e2949d052945b88eb6ecc02da3bcd133f58341e8
indahputris <indahputrisnh@gmail.com>
2021-11-15 16:59:20 +0700

    tugas pert 9 & 10

--- application/views/templates/topbar.php
+++ application/views/templates/topbar.php
@@ -5,7 +5,7 @@
         <!-- Topbar -->
         <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
             <!-- Sidebar Toggle (Topbar) -->
-            <button id="sidebarToggleTop" class="btn btn-link d-mdnone rounded-circle mr-3">
+            <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
                 <i class="fa fa-bars"></i>
             </button>
             <!-- Page Heading -->
@@ -16,8 +16,8 @@
                 <!-- Nav Item - User Information -->
                 <li class="nav-item dropdown no-arrow">
                     <a class="nav-link dropdown-toggle" href="#"
-                    id="userDropdown" role="button" data-toggle="dropdown" ariahaspopup="true" aria-expanded="false">
-                    <span class="mr-2 d-none d-lg-inline textgray-600 small"><?= $user['nama']; ?> </span>
+                    id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $user['nama']; ?> </span>
                     <img class="img-profile rounded-circle"src="<?= base_url('assets/img/profile/') . $user['image']; ?>">
                 </a>
                 <!-- Dropdown - User Information -->
@@ -26,8 +26,8 @@
                         <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400">
                         </i>Profile Saya</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="<?=base_url('autentifikasi/logout'); ?>" data-toggle="modal" datatarget="#logoutModal">
-                        <i class="fas fa-sign-out-alt fa-sm fafw mr-2 text-gray-400"></i>Logout
+                    <a class="dropdown-item" href="<?=base_url('autentifikasi/logout'); ?>" data-toggle="modal" data-target="#logoutModal">
+                        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>Logout
                     </a>
                 </div>
             </li>
